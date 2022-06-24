a0dd97b84d2735c49bc53e668441bf84e23050e9
julien <julien_berry@yahoo.fr>
2022-06-24 09:27:26 +0200

    Après fichiers bat

--- src/glasshousemanager/utils/Logger.java
+++ src/glasshousemanager/utils/Logger.java
@@ -1,4 +1,9 @@
 package glasshousemanager.utils;
 
+import java.time.LocalDateTime;
+
 public class Logger {
+    public void log(String logMessage) {
+        System.out.println("(" + LocalDateTime.now() + ") " + logMessage);
+    }
 }
